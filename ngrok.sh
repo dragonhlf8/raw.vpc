1a644128aef13fa14093dd8e5515d3732c23f07d
dragonhlf8 <138527763+dragonhlf8@users.noreply.github.com>
2024-08-29 15:07:21 +0200

    Add files via upload

---
+++ ngrok.sh
@@ -0,0 +1,13 @@
+wget -nc -O ng.sh https://raw.githubusercontent.com/kmille36/Docker-Kali-Desktop-NoMachine/main/ngrok.sh > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+clear
+./ngrok authtoken 2FYTOkfQl406ckmTpOK8fUtcoTt_4TQFTUpbPRYgy61vC2Ugx
+echo "Go to: https://dashboard.ngrok.com/get-started/your-authtoken"
+clear
+./ngrok tcp --region eu 4000 &>/dev/null &
+sleep 1
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
+clear
+echo IP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
---
+++ user.sh
@@ -0,0 +1,2 @@
+sudo /etc/NX/nxserver --startup
+tail -f /usr/NX/var/log/nxserver.log
